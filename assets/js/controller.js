37013e1ff711969dd58846f06cf0e7c2335bdedc
Glenn Streiff <gsnospampls@pobox.com>
2019-04-10 23:24:37 -0500

    add ability to select various quizzes from selection list
    This updates the quiz in the model and also the background image
    in the view.

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -11,6 +11,13 @@ function QuizController(timeRemainingId, questionId, choicesId, responseId, resu
     // this.countdownInterval = undefined;
     this.addMenuEventListeners();
     this.setTimeRemaining();
+    this.addQuizSelection();
+}
+
+QuizController.prototype.reset = function() {
+    // ... other controller reset things here!
+    this.setQuizBackground();
+    this.qm.reset();
 }
 
 QuizController.prototype.setTimeRemaining = function() {
@@ -18,7 +25,6 @@ QuizController.prototype.setTimeRemaining = function() {
 }
 
 QuizController.prototype.addMenuEventListeners = function() {
-    
     let playId = document.getElementById("navbar-btn");
     playId.addEventListener('click', this.getPlayMenuEventCallback(), false);
 
@@ -37,16 +43,7 @@ QuizController.prototype.getPlayMenuEventCallback = function() {
 
             let cHtml = that.qm.quiz.getChoicesHtml(qi);
             that.choicesId.innerHTML = cHtml;
-
-            console.log("question html: ", qHtml);
-            console.log("choices html: ", cHtml);
         }
-        // if (that.qm.state == "playing") {
-        //     console.log("QuizController.getPlayMenuEventCallback: playing");
-        // } else {
-        //     console.log("QuizController.getPlayMenuEventCallback: !playing");
-        // }
-        // that.setFocus();
     }
     return menuCallback;
 }
@@ -61,4 +58,42 @@ QuizController.prototype.getHelpMenuEventCallback = function() {
         });
     }
     return menuCallback;
+}
+
+QuizController.prototype.addQuizSelection = function() {
+    let pid = document.getElementById("navbar-brand");
+    let sid = document.createElement("select");
+    sid.setAttribute("id", "quiz-select");
+    let quizNames = this.qm.factory.getQuizKeys();
+    for (let name of quizNames) {
+        let oid = document.createElement("option");
+        oid.textContent = name;
+        sid.appendChild(oid);
+    }
+    pid.appendChild(sid);
+    // Let default model quiz drive the initial display.
+    let quizIndex = quizNames.indexOf(this.qm.quiz.quizName);
+    sid.selectedIndex = quizIndex;
+    this.addQuizSelectionListener();
+}
+
+QuizController.prototype.addQuizSelectionListener = function() {
+    var that = this;
+    // TODO: Change this to vanilla js :)
+    $(document).on("change", "#quiz-select", function(e) {
+        console.log("cb selecting quiz: ", $(this).val());
+        that.qm.setQuiz($(this).val());
+        that.reset(); // reset controller and game model
+    })
+}
+
+QuizController.prototype.setQuizBackground = function() {
+    let id = document.getElementById("main");
+    let imgSrc = this.qm.quiz.getImgSrc();
+    if (id) {
+        id.setAttribute("style", 
+            `background: url('${imgSrc}') 0 / cover fixed; 
+            background-position: center center;`
+        );
+    }
 }
